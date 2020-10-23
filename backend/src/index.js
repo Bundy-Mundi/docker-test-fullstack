a6cccf881b50e9230b75d69edbb561e1c3a37346
Ben <bennie.ben095@gmail.com>
2020-10-23 22:09:12 +0900

    Add more codes on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,29 @@ const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
-app.get("/", (req, res)=>{
-    db.pool.query('');
+
+// Create DB Table
+db.pool.query(`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+)`, (err, results, fields) => {
+    if (err) console.log(err)
+    else console.log(results)
+})
+
+app.get("/api/values", function(req, res){
+    db.pool.query(`SELECT * FROM lists;`, (err, results, fields) => {
+        if(err) return res.status(500).send(err)
+        else return res.json.status(200).send(results)
+    });
 });
-app.post("/", (req, res)=>{
-    return "Yes!"
+app.post("/api/values", function(req, res){
+    console.log(req.body.value);
+    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}");`, (err, results) => {
+        if(err) return res.status(500).send(err)
+        else return res.status(200).json({ success: true })
+    })
 });
 
 app.listen(()=>console.log(`Server Listening On: ${PORT}`));
