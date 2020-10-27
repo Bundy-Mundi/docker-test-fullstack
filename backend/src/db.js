d062895e7e1113482d92766ef703d3cd51162668
Ben <bennie.ben095@gmail.com>
2020-10-27 20:48:52 +0900

    AWS deploy conf

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,9 +1,10 @@
 const db = require('mysql');
 const pool = db.createPool({
     connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: 'bennie',
-    database: "myapp"
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE || "myapp",
+    port: process.env.MYSQL_PORT || 3306
 });
 exports.pool = pool;
